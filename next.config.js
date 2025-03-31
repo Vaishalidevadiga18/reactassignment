99df8586125fcee63e9c4fca3db6d06f71f24dba
Varshacshetty <168252322+Varshacshetty@users.noreply.github.com>
2025-03-31 18:28:00 +0100

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      domains: ['image.tmdb.org', 'covers.openlibrary.org'],
+    },
+  };
+  
+  module.exports = nextConfig;
+  
